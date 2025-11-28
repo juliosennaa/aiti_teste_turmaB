ebd8e6016762d7e44d092db9aaa63f8ab9855c2c
juliosennaa <82446923+juliosennaa@users.noreply.github.com>
2025-11-28 13:17:28 +0000

    Aula 28/11

---
+++ exemplo07/classifica_idade.py
@@ -0,0 +1,13 @@
+def classifica_idade(idade):
+    if idade < 12:
+        return "Criança"
+
+    elif idade < 18:
+        return "Adolescente"
+
+    elif idade <60:
+        return "Adulto"
+
+    else:
+        return "Idoso"
+    
